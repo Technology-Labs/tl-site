91e2b4212fe6338dd5eb5843c55adff50a29e9bb
gichimux <gichimumwai1@gmail.com>
2021-02-05 10:17:49 +0300

    remove unused dependencies

--- main/settings.py
+++ main/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 from decouple import config,Csv
-import dj_database_url
+# import dj_database_url
 # import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -29,7 +29,7 @@ MODE=config("MODE", default="dev")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['https://technolabs.co.ke']
+ALLOWED_HOSTS = ['https://technolabs.co.ke', '127.0.0.1']
 
 
 # Application definition
@@ -105,8 +105,8 @@ DATABASES = {
 
 }
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
